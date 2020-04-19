7a77df56a9c251a2e45c0baed9a1b6bac7946396
DazzlingFame <narsel1995@mail.ru>
2020-04-19 20:09:00 +0300

    getTodos method without db

--- server.ts
+++ server.ts
@@ -1,14 +1,25 @@
+// @ts-ignore
 import bodyParser from "body-parser";
+// @ts-ignore
 import express from "express";
 
 const app = express();
 const port = 8000;
+
+const todos = [
+    {id: 1, text: 'note 1', checked: false},
+    {id: 2, text: 'note 2', checked: false},
+];
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/api/headerText', (req, res) => {
     res.send({text: 'Hello mister Freeman'});
+});
+
+app.get('/api/getTodos', (req, res) => {
+    res.send({todos: todos});
 });
 
 app.post('/api/headerPost', (req, res) => {
